b847c07b7bb079054cca644ed0f8afe76f9cbc51
cb <csbutton01@hotmail.com>
2018-03-23 14:53:00 -0700

    display text relating to previous experience

--- js/scripts.js
+++ js/scripts.js
@@ -7,13 +7,25 @@ $(document).ready(function(){
     $("#intro").toggle();
     $("#name-insert").text(name);
   });
+
+  $("#prev-xp").change(function(event){
+    event.preventDefault();
+
+    var prevXp = $(this).find("option:selected").val();
+
+    if (prevXp === "yes") {
+      $("#if-yes").show();
+      $("#if-no").hide();
+    } else if (prevXp === "no") {
+      $("#if-no").show();
+      $("#if-yes").hide();
+    }
+  });
+
   $("#get-started").click(function(event){
     event.preventDefault();
     $("#intro").hide();
     $("#second-card").show();
-
-    
-
     $("#appeal").click(function(event){
     var choiceResult = $("input:radio[name = front-or-back]:checked").val(); //will return which one was checked!
 
